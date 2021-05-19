500f28b7acf0e70a8e5f588e385d6fd149d30f47
Gabriel S Lobin <gabrielsuassuna@hotmail.com>
2021-05-19 18:33:21 -0300

    Organização da pasta de estudos

---
+++ exercises/4-Bloco/exercícios_JS/script.js
@@ -0,0 +1,24 @@
+// * part 1
+// const name = "Gabriel suassuna Amorim";
+// const birthCity = "Natal/RN";
+// const birthYear = "1995";
+// * part 2
+// console.log(name);
+// console.log(birthCity);
+// console.log(birthYear);
+// * part 3
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+// * part 4
+// let base = 5;
+// let altura = 8;
+// let area = base * altura;
+// let perimetro = ((base**2) + (altura * 2)) ;
+// console.log(area);
+// console.log(perimetro);
+// * part 5

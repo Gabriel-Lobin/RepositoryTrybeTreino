7cb17fd516416a81db2f5e78f2852ded0b054fad
Gabriel S Lobin <gabrielsuassuna@hotmail.com>
2021-05-20 17:46:26 -0300

    Exercicio bloco 4 dia 1 atualizados

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ******************************************************************
 ---------------------------------------------
-Gabriel Lobin / Last Att ( 7-05-2021 ).;
+Gabriel Lobin / Last Att ( 19-05-21 ).;
 ---------------------------------------------
 1 - Aprendendo Bash e Unix ( 05-05-21 / 06-05-21 ).;
 
@@ -8,6 +8,9 @@ Gabriel Lobin / Last Att ( 7-05-2021 ).;
 
 3 - Continuação Git / Github "cópia, push e pull" ( 10-05-21 ).;
 
+4 - Aprendendo Html / Css "comandos e semanticas" ( 11-05-21).;
+
+5 - Aprendendo JavaScript " comandos iniciais " ( 19-05-21 ).;
 
 #VQV; 
 ******************************************************************
--- exercises/4-Bloco/dia_1/TreinoJS.js
+++ exercises/4-Bloco/dia_1/TreinoJS.js
@@ -5,6 +5,7 @@
 // Multiplicação (a * b)
 // Divisão (a / b)
 // Módulo (a % b)
+
 //-----------------------------------------
 
 // let a = 5;
@@ -15,7 +16,8 @@
 // console.log(a*b);
 // console.log(a/b);
 // console.log(a%b);
-// 
+
+//-----------------------------------------
 
 // Questão 2 = Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
 
@@ -31,6 +33,8 @@
 // } else {
 // 	console.log('Numeros iguais');
 // }
+
+//-----------------------------------------
 
 // Questão 3 = Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
 
@@ -49,6 +53,8 @@
 // 	console.log('todos iguais');
 // }
 
+//-----------------------------------------
+
 // Questão 4 = Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
 //-----------------------------------------
@@ -63,6 +69,8 @@
 // 	console.log('Zero');
 // }
 
+//-----------------------------------------
+
 // Questão 5 = Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
 // Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
@@ -76,11 +84,15 @@
 
 // let triangulo = angulo1 + angulo2 + angulo3;
 
-// if ( triangulo == 180){
-// 	console.log(triangulo + ' True');
+// if ( angulo1 < 0 || angulo2 < 0 || angulo3 < 0 ) {
+// 	console.log('angulo inválido')
+// } else if ( triangulo == 180){
+// 	console.log(triangulo + ' é True');
 // } else {
-// 	console.log(triangulo + ' False');
+// 	console.log(triangulo + ' é False');
 // }
+
+//-----------------------------------------
 
 // Questão 6 = Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 
@@ -91,10 +103,10 @@
 
 //-----------------------------------------
 
-// let peçaXadrez = 'peão';
+// let peçaXadrez = 'Cavalo';
+// let peça = peçaXadrez.toLowerCase();
 
-
-// switch (peçaXadrez){
+// switch (peça){
 // 	case 'peão':
 // 		console.log('Avance uma casa'); 
 // 		break;
@@ -116,6 +128,8 @@
 // 		default:
 // 			console.log('Erro Catastrófico');
 // }
+
+//-----------------------------------------
 
 // Questão 7 = Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
 
@@ -147,6 +161,8 @@
 // 	console.log('F');
 // }
 
+//-----------------------------------------
+
 // Questão 8 = Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false . 
 
 // Bonus: use somente um if .
@@ -162,6 +178,8 @@
 // } else {
 // 	console.log('false');
 // }
+
+//-----------------------------------------
 
 // Questão 9 = Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false . Bonus: use somente um if .
 
@@ -179,6 +197,8 @@
 // 	console.log('false');
 // }
 
+//-----------------------------------------
+
 // Questão 10 = Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
 
 // tente que, sobre o custo do produto, incide um imposto de 20%.
@@ -189,6 +209,56 @@
 
 //-----------------------------------------
 
-let custo = 50;
-let valor = 100;
+// let custo = 50;
+// let venda = 100;
 
+// let custoTotal = custo + custo * 0.2;
+
+// let lucro = venda - custoTotal;
+// let lucroX1000 = lucro * 1000;
+
+// if (custo < 0 || venda < 0){
+// 	console.log('Erro Catastrófico');
+// } else {
+//  	console.log('o lucro desta empresa é R$ ' + lucro + ', e ao vender mil produtos é R$ ' + lucroX1000 + '.' );
+// }
+
+//-----------------------------------------
+
+// Questão 11 = Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+
+// A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+// INSS (Instituto Nacional do Seguro Social)
+// Salário bruto até R$ 1.556,94: alíquota de 8%
+// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+// IR (Imposto de Renda)
+// Até R$ 1.903,98: isento de imposto de renda
+// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+// Exemplo : Uma pessoa possui o salário bruto de R$ 3.000,00. O cálculo será:
+// O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
+// Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
+// Para pegar o valor do IR, temos um salário (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, sendo a alíquota, então, de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. Assim, temos:
+// R$ 2.670,00: salário com INSS já deduzido;
+// 7.5%: alíquota de imposto de renda;
+// R$ 142,80 parcela a se deduzir do imposto.
+// Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
+// O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
+// Resultado: R$ 2.612,55.
+// Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?
+
+//-----------------------------------------
+
+
+
+
+
+
+
+
+
+//-----------------------------------------

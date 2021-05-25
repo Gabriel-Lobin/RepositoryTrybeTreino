3da1d5040efce4fabdb834a5072ebef50833622b
Gabriel S Lobin <gabrielsuassuna@hotmail.com>
2021-05-24 22:56:21 -0300

    Exercicios Bloco 4 Dia 4

--- exercises/4-Bloco/dia_1/TreinoJS.js
+++ exercises/4-Bloco/dia_1/TreinoJS.js
@@ -233,12 +233,14 @@
 // Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
 // Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
 // Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+// 
 // IR (Imposto de Renda)
 // Até R$ 1.903,98: isento de imposto de renda
 // De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
 // De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
 // De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
 // Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+// 
 // Exemplo : Uma pessoa possui o salário bruto de R$ 3.000,00. O cálculo será:
 // O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
 // Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
@@ -252,6 +254,39 @@
 // Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?
 
 //-----------------------------------------
+
+// let salarioBruto = 3000.00;
+// let aliquota;
+
+// if ( salarioBruto <= 1556.94){
+//     aliquota =  salarioBruto * 0.08; 
+// } else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.94){
+//     aliquota = salarioBruto * 0.09;
+// } else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82){
+//     aliquota = salarioBruto * 0.11;
+// } else {
+//     aliquota = 570.88;
+// }
+
+// let salarioAliquota = salarioBruto - aliquota;
+// let ir;
+// let salarioIr;
+
+// if (salarioAliquota < 1903.98){
+//     salarioIr = salarioAliquota;
+// } else if ( salarioAliquota >= 1903.99 && salarioAliquota <= 2826.65 ){
+//     ir = salarioAliquota * 0.075 - 142.80;
+// } else if ( salarioAliquota >= 2826.66 && salarioAliquota <= 3751.05 ){
+//     ir = salarioAliquota * 0.15 - 354.80;
+// } else if ( salarioAliquota >= 3751.06 && salarioAliquota <= 4664.68 ){
+//     ir = salarioAliquota * 0.225 - 636.13; 
+// } else if (salarioAliquota >= 4664.69){
+//     ir = salarioAliquota * 0.275 - 869.36;
+// }
+ 
+//     salarioIr = salarioAliquota - ir; 
+
+// console.log('O salário bruto menos imposto de renda e inss é = ' + salarioIr);
 
 
 

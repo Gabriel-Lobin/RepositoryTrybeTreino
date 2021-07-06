81c51da7c2e75ebcb1e3f0aa6af97ee54e2c7ad3
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-07-06 19:34:18 -0300

    exercicio Bloco 9 Dia 1

---
+++ exercises/9-Bloco/dia_1/exercise4.js
@@ -0,0 +1,17 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = (delay, MarsTemp) => {
+  setTimeout(() => {
+    console.log(`Mars temperature is : ${MarsTemp} degree Celsius`);
+  }, delay);
+}
+
+
+sendMarsTemperature(messageDelay(), getMarsTemperature()); // imprime "Mars temperature is: 20 degree Celsius", por exemplo

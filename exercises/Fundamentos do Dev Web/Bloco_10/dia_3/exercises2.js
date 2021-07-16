185c0221217236c2f7246f1a2f51b92e56c8bd81
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-07-16 17:39:26 -0300

    exercicios

--- exercises/Fundamentos do Dev Web/Bloco_10/dia_3/exercises2.js
+++ exercises/Fundamentos do Dev Web/Bloco_10/dia_3/exercises2.js
@@ -1,8 +1,18 @@
+const fetch = require('node-fetch');
 
 const randomNumber = () => Math.floor(Math.random() * 100);
 
+const caixaAlta = (str) => str.toUpperCase();
 
+const firstLetter = (string) => string[0];
 
+const concatt = (a, b) => `${a} ${b}`;
 
+const randomDog = () => {
+  const url = 'https://dog.ceo/api/breeds/image/random';
+  fetch(url)
+  .then((resolve) => resolve.json())
+  .then((respost) => respost.status);
+};
 
-module.exports = { randomNumber }
+module.exports = { randomNumber, caixaAlta, firstLetter, concatt, randomDog }
--- exercises/Fundamentos do Dev Web/Bloco_10/dia_3/exercises2.test.js
+++ exercises/Fundamentos do Dev Web/Bloco_10/dia_3/exercises2.test.js
@@ -1,5 +1,5 @@
 const functions = require('./exercises2');
-const { randomNumber } = functions;
+const { randomNumber, caixaAlta, firstLetter, concatt, randomDog } = functions;
 
 describe('testa functions', () => {
   test('randomNumber', () => {
@@ -21,10 +21,37 @@ describe('testa functions', () => {
     expect(random).toReturn();
     expect(random).toHaveBeenCalledWith(2, 1);
   })
-  test('test division', () => {
-    // expect.assertions(3);
-
+  test('division', () => {
+    expect.assertions(3);
     const random = jest.spyOn(functions, 'randomNumber')
-      .mockImplementationOnce()
+      .mockImplementation((a, b, c) => a * b * c);
+    random();
+    expect(random).toHaveBeenCalled();
+    expect(random).toReturn();
+    random.mockReset();
+    random.mockImplementation((a) => a * 2);
+    expect(random(2)).toBe(4);
+  })
+  test('caixaAlta', () => {
+    expect(caixaAlta('a')).toBe('A');
+    const caixa = jest.spyOn(functions, 'caixaAlta')
+      .mockImplementation((str) => str.toLowerCase());
+      expect(caixa('A')).toBe('a');
+  })
+  test('firstLetter', () => {
+    expect(firstLetter('nome grande')).toBe('n');
+    const last = jest.spyOn(functions, 'firstLetter')
+      .mockImplementation((string) => string[string.length - 1])
+      expect(last('nome grande')).toBe('e')
+  })
+  test('concatt', () => {
+    expect(concatt('to', 'loko')).toBe('to loko');
+    const concatt3 = jest.spyOn(functions, 'concatt')
+    .mockImplementation((a, b, c) => `${a} ${b} ${c}`)
+    expect(concatt3('to', 'muito', 'loko')).toBe('to muito loko');
+  })
+  test('randomDog', () => {
+    // randomDog()
+    // .then((dog) => `request ${dog.status}`)
   })
 })
